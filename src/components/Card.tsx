f19c5f9e93667f4f22dbbd4dc4198a70483e9d05
benjaminchin <benjamin.d.chin@gmail.com>
2023-04-11 15:13:55 -0400

    resume change and card position

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -37,7 +37,7 @@ const Card = ({
         {!hasVideo && (
           <img
             src={`/assets/${img}`}
-            className="w-[300px] h-[150px] object-cover"
+            className="w-[300px] h-[150px] object-cover object-top"
             alt="img"
           />
         )}
